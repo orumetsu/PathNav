9cca90a3644f66df7b2d491042ba40331a4dbca9
Firmansyah Sandy <firmansyahsandy100@gmail.com>
2021-09-06 21:19:54 +0700

    edit soal scene

--- javascript/game/objects/formAnswer.js
+++ javascript/game/objects/formAnswer.js
@@ -1,55 +1,61 @@
 
 export function formAnswer(level,widthQuestion,heightQuestion) {
 
-    let titleForm =  'Your Answer : ';  
+    let titleForm =  'Input Your Answer';  
     let padding = 20;
     let sizeFont = 14;
-    let posYForm  = heightQuestion +((height() - heightQuestion) * 0.5);
-    let posXForm = padding + ( sizeFont * titleForm.split('').length);
-    let posYButtonSubmit = posYForm;
-    let posXButtonSubmit = width() - padding;
+    let posYInfo  = heightQuestion +((height() - heightQuestion) * 0.5);
+    let posXInfo = padding;
+    let posYButtonNFormSubmit = posYInfo;
+    let posXButtonNFormSubmit = width() - padding;
     let FormIsFilled = false;
 
-        const button = add([
-            pos(vec2(posXButtonSubmit,posYButtonSubmit)),
-            rect(100,50),
-            origin("right")
-            ,color(0,0,0)]);
-
-        const titleText = add([
-            text("submit", 13),
-            pos(vec2(posXButtonSubmit - 50,posYButtonSubmit)),
-            origin("center"),
-            color(1, 1, 1),
-        ]);
-
-        button.action(() => {
-            if(FormIsFilled){
-                if (button.isHovered()) {
-                    button.color = rgb(1, 1, 1);
-                    titleText.color = rgb(0,0,0);
-                    if (mouseIsClicked()) {
-                       
-                    }
-                } else {
-                    titleText.color = rgb(1,1,1);
-                    button.color = rgb(0, 0, 0);
-                }
-            }
-        })
-    
+    add([
+        text("B = +8 \n \nK =  0 \n \nS = -4", 14),
+        pos(vec2(posXInfo,posYInfo)),
+        origin("left"),
+        color(0, 0, 0),
+    ]);
 
     const form = add([
         text(titleForm, sizeFont),
-        pos(vec2(posXForm,posYForm)),
+        pos(vec2(posXButtonNFormSubmit ,posYButtonNFormSubmit)),
         origin("right"),
         color(0, 0, 0),
     ]);
 
+    const button = add([
+        pos(vec2(posXButtonNFormSubmit,posYButtonNFormSubmit)),
+        rect(100,50),
+        origin("right")
+        ,color(0,0,0)]);
+
+    const titleText = add([
+        text("submit", 13),
+        pos(vec2(posXButtonNFormSubmit - 50,posYButtonNFormSubmit)),
+        origin("center"),
+        color(1, 1, 1),
+    ]);
+
+    button.action(() => {
+        if(FormIsFilled){
+            if (button.isHovered()) {
+                button.color = rgb(0.8, 0.8, 0.8);
+                titleText.color = rgb(0,0,0);
+                if (mouseIsClicked()) {
+                   
+                }
+            } else {
+                titleText.color = rgb(1,1,1);
+                button.color = rgb(0, 0, 0);
+            }
+        }
+    })
+
     const answer = add([
         text('', 14),
-        pos(vec2(posXForm,posYForm)),
-        origin("left"),
+        pos(vec2(posXButtonNFormSubmit - 120,posYButtonNFormSubmit)),
+        origin("right"),
         color(0, 0, 0),
     ]);
 
@@ -64,12 +70,14 @@ export function formAnswer(level,widthQuestion,heightQuestion) {
     answer.action(()=>{
         if(answer.text == ''){
             FormIsFilled = false;
-            titleText.color = rgb(1,1,1);
-            button.color = rgb(1, 1, 1);
+            titleText.hidden = true;
+            button.hidden = true;
+            form.hidden = false;
         }else{
             FormIsFilled = true;
-            titleText.color = rgb(1,1,1);
-            button.color = rgb(0, 0, 0);
+            titleText.hidden = false;
+            button.hidden = false;
+            form.hidden = true;
         }
     });
 
